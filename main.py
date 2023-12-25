ea798b988f01937b5202833eb40c7b8e7b1fb691
rohitbkbi <rohitamekar@gmail.com>
2023-12-25 15:08:31 +0530

    Flies updated

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ Counter({"ham": 3, "roast beef": 2, "cheese": 2})
 
 sandwiches = ["ham", "cheese", "roast beef", "ham", "cheese", "roast beef", "ham"]
 c = Counter(sandwiches)
+
+ print("There are {} ham sandwiches".format(c["ham"]))
+There are 3 ham sandwiches
+
+Counter({"ham": 3, "roast beef": 2, "cheese": 2})
+
+sandwiches = ["ham", "cheese", "roast beef", "ham", "cheese", "roast beef", "ham"]
+c = Counter(sandwiches)
+
+ print("There are {} ham sandwiches".format(c["ham"]))
+There are 3 ham sandwiches
+
+Counter({"ham": 3, "roast beef": 2, "cheese": 2})
+
+sandwiches = ["ham", "cheese", "roast beef", "ham", "cheese", "roast beef", "ham"]
+c = Counter(sandwiches)
