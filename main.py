3264c36eb4e973786b2cf8c41988eb8442246827
rohitbkbi <rohitamekar@gmail.com>
2023-12-25 14:27:36 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,48 +1,15 @@
-# This is a sample Python script.
+ print("There are {} ham sandwiches".format(c["ham"]))
+There are 3 ham sandwiches
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Counter({"ham": 3, "roast beef": 2, "cheese": 2})
 
+sandwiches = ["ham", "cheese", "roast beef", "ham", "cheese", "roast beef", "ham"]
+c = Counter(sandwiches)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ print("There are {} ham sandwiches".format(c["ham"]))
+There are 3 ham sandwiches
 
+Counter({"ham": 3, "roast beef": 2, "cheese": 2})
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+sandwiches = ["ham", "cheese", "roast beef", "ham", "cheese", "roast beef", "ham"]
+c = Counter(sandwiches)
